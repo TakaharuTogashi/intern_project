3c4cfab943083f33595c8f83e95988893e9257e0
TakaharuTogashi <intern@NEKO-097.local>
2015-05-25 12:52:16 +0900

    パスワード生成機能を実装

--- test.rb
+++ test.rb
@@ -7,3 +7,10 @@ inum.times{
 	puts account + "@backstore.jp"
 }
 
+password_words = ((2..9).to_a + ("a".."k").to_a + ("m".."z").to_a
+				+ ("A".."H").to_a + ("J".."N").to_a + ("P".."Z").to_a)
+
+inum.times{
+	password = password_words.sample(8).join
+	puts password
+}
