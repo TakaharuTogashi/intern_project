12f2869311bf1c59c2d73fa202a896094cde17bd
TakaharuTogashi <intern@NEKO-097.local>
2015-05-25 16:43:10 +0900

    重複があった場合に解消する処理を追加

--- test.rb
+++ test.rb
@@ -2,17 +2,25 @@ inum = ARGV[0].to_i
 
 account_words = ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a)
 password_words = ((2..9).to_a + ("a".."k").to_a + ("m".."z").to_a + ("A".."H").to_a + ("J".."N").to_a + ("P".."Z").to_a)
-users = []
 
-inum.times{
-	account = account_words.sample(8).join + "@backstore.jp"
-	password = password_words.sample(8).join
-	user = {:account => account, :password => password}
-	users << user
-}
+loop do
+	users = []
 
-File.open('test_account.csv', 'w') do |fh|
-	users.each do |user|
-		fh.write(user[:account] + "," + user[:password] + "\n")
+	inum.times do
+		account = account_words.sample(8).join + "@backstore.jp"
+		password = password_words.sample(8).join
+		user = {:account => account, :password => password}
+		users << user
+	end
+
+	File.open('test_account.csv', 'w') do |fh|
+		users.each do |user|
+			fh.write(user[:account] + "," + user[:password] + "\n")
+		end
+	end
+
+	avoid_repetition = users.uniq
+	if avoid_repetition == users
+		break
 	end
 end
