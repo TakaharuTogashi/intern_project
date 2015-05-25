f53901581268d4ff94514743d9f1c386935d7c9b
TakaharuTogashi <intern@NEKO-097.local>
2015-05-25 15:29:03 +0900

    user情報をusersに保存するよう変更

--- test.rb
+++ test.rb
@@ -1,15 +1,16 @@
 inum = ARGV[0].to_i
 
 account_words = ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a)
-
-inum.times{
-	account = account_words.sample(8).join
-	puts account + "@backstore.jp"
-}
-
 password_words = ((2..9).to_a + ("a".."k").to_a + ("m".."z").to_a + ("A".."H").to_a + ("J".."N").to_a + ("P".."Z").to_a)
-
+users = []
 inum.times{
+	account = account_words.sample(8).join + "@backstore.jp"
+	# File.write('test_account.csv', account + ", ")
+
 	password = password_words.sample(8).join
-	puts password
+	# File.write('test_account.csv', password + "/n")
+
+	user = {:account => account, :password => password}
+	users << user
 }
+puts users
