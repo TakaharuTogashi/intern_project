ae3e6fafaeecc3220881a0d8f605e9c1fea52e89
TakaharuTogashi <intern@NEKO-097.local>
2015-05-25 15:43:03 +0900

    csvファイルの書き出しを実装

--- test.rb
+++ test.rb
@@ -3,14 +3,16 @@ inum = ARGV[0].to_i
 account_words = ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a)
 password_words = ((2..9).to_a + ("a".."k").to_a + ("m".."z").to_a + ("A".."H").to_a + ("J".."N").to_a + ("P".."Z").to_a)
 users = []
+
 inum.times{
 	account = account_words.sample(8).join + "@backstore.jp"
-	# File.write('test_account.csv', account + ", ")
-
 	password = password_words.sample(8).join
-	# File.write('test_account.csv', password + "/n")
-
 	user = {:account => account, :password => password}
 	users << user
 }
-puts users
+
+File.open('test_account.csv', 'w') do |fh|
+	users.each do |user|
+		fh.write(user[:account] + "," + user[:password] + "\n")
+	end
+end
