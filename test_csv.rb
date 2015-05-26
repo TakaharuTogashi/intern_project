9ad3dba6c7a3feb86fc44a6d820a4153f9598b0a
TakaharuTogashi <intern@NEKO-097.local>
2015-05-26 11:36:49 +0900

    アカウントの仕様をテストするプログラム完成

--- test_csv.rb
+++ test_csv.rb
@@ -1,10 +1,14 @@
 require 'csv'
+valid_account = []
+invalid_account = []
 
 CSV.foreach(ARGV[0]) do |account, password|
-	if account.length == 21 && /@backstore.jp/ =~ account && password.length == 8 &&/[lIO10]/ !~ password
-		puts 1
+	if account.length == 21 && /^[a-zA-Z0-9]/ =~ account && /\@backstore\.jp$/ =~ account && /(.)\1\1/ !~ account && password.length == 8 && /(.)\1\1/ !~ password && /[lIO10][^a-zA-Z0-9]/ !~ password then
+		valid_account << account + "," + password
 	else
-		puts '0   ' + account + password
+		invalid_account	<< account + "," + password
 	end
-
 end
+
+puts "仕様を満たすアカウント数　　　　#{valid_account.length}"
+puts "仕様を満たさないアカウント数　　#{invalid_account.length}"
